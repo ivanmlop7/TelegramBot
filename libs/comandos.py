9843309816de9d24c240fc9a1fd9989547f2cf53
ivanmlop7 <ivandosamv@gmail.com>
2023-07-07 21:43:35 +0200

    mejorada funcionalidad agregar productos

--- libs/comandos.py
+++ libs/comandos.py
@@ -1,7 +1,8 @@
+import json
+import telebot
+import emoji
 from configs.secrets import token
 from libs.mensajes import mensaje_help
-import telebot
-import json
 
 bot = telebot.TeleBot(token)
 shopping_list = 'data/shopping_list.json'
@@ -29,3 +30,35 @@ class Commands:
         with open(shopping_list, "w") as file:
             json.dump({}, file)
         bot.reply_to(message, 'Se ha borrado el contenido de la lista')
+
+    # @staticmethod
+    # def add_product_to_list(message):
+    #     bot.send_message(message.chat.id, 'Agrega productos a la lista. Una vez finalizado usa el comando /end para salir del modo de agregación.')
+
+    #     # Función para manejar el siguiente mensaje
+    #     def handle_next_step(next_message):
+    #         if next_message.text == '/end':
+    #             bot.send_message(next_message.chat.id, 'Modo de agregación finalizado.')
+    #         elif not next_message.text.startswith("/"):
+    #             with open(shopping_list, "r") as archivo:
+    #                 datos = json.load(archivo)
+    #             if 'Productos' in datos:
+    #                 datos["Productos"].append(next_message.text)
+    #             else:
+    #                 datos["Productos"] = [next_message.text]
+
+    #             with open(shopping_list, 'w') as archivo:
+    #                 json.dump(datos, archivo)
+
+    #             # Responder con un emoji
+    #             emoji_producto_agregado = "\u2705"
+    #             bot.send_message(next_message.chat.id, emoji_producto_agregado)
+
+    #             # Espera al siguiente mensaje
+    #             bot.register_next_step_handler(next_message, handle_next_step)
+    #         else:
+    #             bot.send_message(next_message.chat.id, 'El producto no puede empezar con "/".')
+        
+    #     # Espera al siguiente mensaje
+    #     bot.register_next_step_handler(message, handle_next_step)
+        
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from configs.secrets import token
-from libs.comandos import Commands
 import telebot
 import json
+from configs.secrets import token
+from libs.comandos import Commands
+
 
 bot = telebot.TeleBot(token)
 cmd = Commands()
@@ -20,25 +21,50 @@ def cmd_handler(message):
 
 
 @bot.message_handler(commands=['clear', 'borrar'])
-def cmd_list(message):
+def cmd_handler(message):
     cmd.delete_list(message)
 
+# @bot.message_handler(commands=['add', 'agregar'])
+# def cmd_handler(message):
+#     cmd.add_product_to_list(message)    
 
-# Cambiar esta entrada por un comando para agregar elementos
-@bot.message_handler(content_types=["text"])
+
+@bot.message_handler(commands=['add', 'agregar'])
 def bot_mensajes_texto(message):
     if message.text.startswith("/"):
-        bot.send_message(message.chat.id, 'Comando no disponible')
-    else:
-        with open(shopping_list, "r") as archivo:
-            datos = json.load(archivo)
-        if 'Productos' in datos:
-            datos["Productos"].append(message.text)
-        else:
-            datos["Productos"] = [message.text]
+        bot.send_message(message.chat.id, 'Agrega productos a la lista. Una vez finalizado usa el comando /end para salir del modo de agregación.')
 
-        with open(shopping_list, 'w') as archivo:
-            json.dump(datos, archivo)
+        # Función para manejar el siguiente mensaje
+        def handle_next_step(next_message):
+            if next_message.text == '/end':
+                bot.send_message(next_message.chat.id, 'Modo de agregación finalizado.')
+            elif not next_message.text.startswith("/"):
+                with open(shopping_list, "r") as archivo:
+                    datos = json.load(archivo)
+                if 'Productos' in datos:
+                    datos["Productos"].append(next_message.text)
+                else:
+                    datos["Productos"] = [next_message.text]
+
+                with open(shopping_list, 'w') as archivo:
+                    json.dump(datos, archivo)
+
+                # Responder con un emoji
+                emoji_producto_agregado = "\u2705"
+                bot.send_message(next_message.chat.id, emoji_producto_agregado)
+
+                # Espera al siguiente mensaje
+                bot.register_next_step_handler(next_message, handle_next_step)
+            else:
+                bot.send_message(next_message.chat.id, 'El producto no puede empezar con "/".')
+
+        # Espera al siguiente mensaje
+        bot.register_next_step_handler(message, handle_next_step)
+    else:
+        bot.send_message(message.chat.id, 'Comando inválido. Por favor, usa el comando /add o /agregar para iniciar el modo de agregación.')
+
+
+
 
 
 if __name__ == '__main__':
