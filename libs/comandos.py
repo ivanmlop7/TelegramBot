9a1dd0c8c35458a88ab962816efb0a28fddbfd46
ivanmlop7 <ivandosamv@gmail.com>
2023-07-31 16:12:38 +0200

    Actualizacion Julio

--- libs/comandos.py
+++ libs/comandos.py
@@ -1,8 +1,11 @@
 import json
 import telebot
+import openai
 import emoji
 from configs.secrets import token
 from libs.mensajes import msg_help, msg_clear_list, msg_start_with, msg_add_product_end, msg_add_product_error_slash, msg_add_product_error
+import requests
+from bs4 import BeautifulSoup
 
 bot = telebot.TeleBot(token)
 shopping_list = 'data/shopping_list.json'
@@ -11,6 +14,7 @@ shopping_list = 'data/shopping_list.json'
 class Commands:
     @staticmethod
     def help(message):
+        print(message)
         bot.reply_to(message, msg_help)
 
     @staticmethod
@@ -31,7 +35,6 @@ class Commands:
         with open(shopping_list, "w") as file:
             json.dump({}, file)
         bot.reply_to(message, msg_clear_list)
-
 
     @staticmethod
     def add_product_to_list(bot, message):
@@ -66,4 +69,60 @@ class Commands:
             bot.register_next_step_handler(message, handle_next_step)
         else:
             bot.send_message(message.chat.id, msg_add_product_error)
+
+    # @staticmethod
+    # def chat_with_gpt(message):
+    #     query = message.text  # Obtén la consulta del mensaje del usuario
+
+    #     # Llama a la API de OpenAI para obtener una respuesta del modelo de ChatGPT
+    #     response = openai.Completion.create(
+    #         engine='text-davinci-003',
+    #         prompt=query,
+    #         max_tokens=50,
+    #         n=1,
+    #         stop=None,
+    #         temperature=0.7
+    #     )
+
+    #     # Extrae la respuesta generada por el modelo
+    #     answer = response.choices[0].text.strip()
+
+    #     # Envía la respuesta al usuario
+    #     bot.reply_to(message, answer)
         
+
+    # @staticmethod
+    # def get_weather_forecast(message):
+    #     concello = message.text.split()[1]  # Obtén el nombre del concello del mensaje
+
+    #     # Construye la URL de consulta utilizando el nombre del concello
+    #     url = f"https://www.meteogalicia.gal/web/predicion/localidades/localidadesIndex.action?request_locale=gl&conc={concello}"
+
+    #     # Realiza la solicitud GET a la URL y obtén el contenido HTML de la página
+    #     response = requests.get(url)
+    #     if response.status_code == 200:
+    #         html_content = response.text
+
+    #         # Analiza el contenido HTML utilizando BeautifulSoup
+    #         soup = BeautifulSoup(html_content, 'html.parser')
+
+    #         # Encuentra y extrae la información de la previsión meteorológica
+    #         prevision_div = soup.find("div", class_="prevision_dia")
+    #         print(prevision_div)
+    #         if prevision_div:
+    #             # Extraer los datos de la previsión meteorológica
+    #             fecha = prevision_div.find("h4").get_text()
+    #             tiempo = prevision_div.find("p").get_text()
+
+    #             # Imprimir los datos extraídos
+    #             print("Fecha:", fecha)
+    #             print("Tiempo:", tiempo)
+
+    #         # ... código para extraer la información ...
+
+    #         # Envía la información de la previsión meteorológica al usuario
+    #         bot.reply_to(message, "Aquí está la previsión meteorológica para el concello de " + concello + ":")
+    #         # ... código para enviar la información ...
+
+    #     else:
+    #         bot.reply_to(message, "No se pudo obtener la previsión meteorológica en este momento.")        
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import telebot
-from configs.secrets import token
+import openai
+from configs.secrets import token, openai_key
 from libs.comandos import Commands
 from libs.mensajes import msg_unauthorized
 from libs.validations import check_user_id
 
 
 bot = telebot.TeleBot(token)
+key = openai.api_key = openai_key
 cmd = Commands()
 
 shopping_list = 'data/shopping_list.json'
@@ -34,6 +36,24 @@ def cmd_handler(message):
 @bot.message_handler(commands=['add', 'agregar'])
 def cmd_handler(message):
     cmd.add_product_to_list(bot, message)
+
+# @bot.message_handler(commands=['chat', 'conversar'])
+# def cmd_handler(message):
+#     cmd = Commands()
+
+#     if message.text.startswith('/chat') or message.text.startswith('/conversar'):
+#         cmd.chat_with_gpt(message)
+#     else:
+#         bot.reply_to(message, 'Comando no reconocido. Por favor, utiliza /chat <consulta> o /conversar <consulta> para conversar con ChatGPT.')
+
+# @bot.message_handler(commands=['weather', 'tiempo'])
+# def cmd_handler(message):
+#     if message.text.startswith('/weather') or message.text.startswith('/tiempo'):
+#         cmd.get_weather_forecast(message)
+#     else:
+#         bot.reply_to(message, 'Comando no reconocido. Por favor, utiliza /weather <concello> o /tiempo <concello> para consultar la previsión meteorológica.')
+
+
 
 
 if __name__ == '__main__':
